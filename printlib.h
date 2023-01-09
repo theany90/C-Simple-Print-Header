20fa198e4b712b4591809fb2ec96143d73a2ca1e
theany90 <106186005+theany90@users.noreply.github.com>
2023-01-09 03:57:37 +0300

    Updated a few lines.
    Changed the function for containers, now it has support for every container that C++ has. There might be a better implementation. I'll try to improve.

--- printlib.h
+++ printlib.h
@@ -3,62 +3,66 @@
 #include <typeinfo>
 #include <cstring>
 template<class Type, size_t N>
-Type print(Type (&data)[N], int opt = 0) {
+void print(Type (&data)[N], int opt = 0) {
     int typeSize = sizeof(Type);
     const size_t size = N;
     for(int i = 0; i < size; i++) {
-        std::cout << *data + i; 
+        std::cout << data[i]; 
         if(opt == 1) {
             std::cout << std::endl;
         } 
     }
-    std::cout.flush();
-    return *data; 
 }
 template<class Type>
-Type print(Type data, int opt = 0) {
+void print(Type data, int opt = 0) {
     if(opt == 0) {
-        std::cout << data << std::flush; 
-        std::cout.flush();
-        return data; 
+        std::cout << data;
+        return; 
     }
     std::cout << data << std::endl; 
-    std::cout.flush();
+
 }
 template<class Type>
-Type printLine(Type data) {
+void printLine(Type data) {
     print(data, 1); 
-    std::cout.flush();
-    return data; 
 }
 template<class Type, size_t N>
-Type printLine(Type (&data)[N]) {
+void printLine(Type (&data)[N]) {
     char * buf; 
     sprintf(buf, "A%d_c", N);
     const char * str = typeid(data).name(); 
     if(strstr(str, buf)) {
         std::cout << data << std::endl;
-        return 'c'; 
+        return; 
     }
     print(data, 1);
-    std::cout.flush();
-    return *data; 
 }
-template <class Type>
-std::vector<Type> print(std::vector<Type> &data, int opt = 0) {
-    auto it = data.begin();
-    for(it; it != data.end(); it++) {
-        std::cout << *it;  
-        if(opt == 1) {
-            std::cout << std::endl; 
+template<typename Container>
+void print(const Container &data,  const std::string &delimiter = "") {
+    if (data.size() == 0) return;
+    for(auto it = data.begin(); it != data.end(); ++it) {
+        std::cout << *it;
+        if(it < data.end() - 1) {
+            std::cout << delimiter;
         }
     }
-    std::cout.flush();
-    return data; 
+    std::cout << std::endl; 
 }
-template <class Type>
-std::vector<Type> printLine(std::vector<Type> data) {
-    print(data, 1);
-    std::cout.flush(); 
-    return data; 
+template <typename Container>
+void printLine(const Container &data, const std::string &delimiter = "") {
+    print(data,  delimiter + "\n");
+}
+template <typename T>
+void print(std::initializer_list<T> data, const std::string &delimiter = "") {
+    if (data.size() == 0) return;  // Return if the initializer list is empty
+    for (auto it = data.begin(); it != data.end(); ++it) {
+        std::cout << *it;
+        if (it < data.end() - 1) {
+            std::cout << delimiter;
+        }
+    }
+}
+template <typename T>
+void printLine(std::initializer_list<T> data, const std::string &delimiter = "") {
+    print(data, delimiter + "\n");
 }
